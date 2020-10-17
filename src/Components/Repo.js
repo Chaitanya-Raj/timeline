d40e89a08479c48ab3124c447adc1cce542b202f
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-10-17 14:55:51 +0530

    format dates

--- src/Components/Repo.js
+++ src/Components/Repo.js
@@ -1,9 +1,38 @@
 import React from "react";
 
 const Repo = ({ repos }) => {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
   return (
     <div className="timeline-container">
       {repos.map((repo) => {
+        const date = new Date(repo.date);
+        let formatted_date = `${days[date.getDay()]}, ${date.getDate()} ${
+          months[date.getMonth()]
+        } ${date.getFullYear()}`;
         return (
           <div className="timeline-item" key={repo.link.url}>
             <div className="timeline-item-content">
@@ -15,7 +44,7 @@ const Repo = ({ repos }) => {
                   {repo.category.tag}
                 </span>
               )}
-              <time>{repo.date}</time>
+              <time>{formatted_date}</time>
               <p>{repo.text}</p>
               {repo.link && (
                 <a
