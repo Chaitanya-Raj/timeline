8391999f6cf32434b307f3a196b301822757c608
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2022-07-12 15:42:53 +0530

    Add login via github

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { graphql } from "@octokit/graphql";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import User from "./Components/User";
 import Repo from "./Components/Repo";
+import logo from "./github_logo.png";
 
-const REACT_APP_TOKEN = process.env.REACT_APP_TOKEN;
+const CLIENT_ID = "f989a2362def54e6d959";
+const REDIRECT_URI = "https://chaitanya-raj.github.io/timeline/";
 
 const App = () => {
-  const [login, setLogin] = useState("");
+  const [token, setToken] = useState(null);
+  const [username, setUsername] = useState("");
   const [user, setUser] = useState(null);
   const [repos, setRepos] = useState(null);
   const first = 100;
+
+  useEffect(() => {
+    const code =
+      window.location.href.match(/\?code=(.*)/) &&
+      window.location.href.match(/\?code=(.*)/)[1];
+    console.log(code);
+    if (code) {
+      fetch(`https://timeline-github.herokuapp.com/authenticate/${code}`)
+        .then((response) => response.json())
+        .then(({ token }) => {
+          console.log(token);
+          setToken(token);
+        });
+    }
+  }, []);
 
   const notify = () =>
     toast("User does not exist", {
@@ -30,8 +48,8 @@ const App = () => {
     try {
       const { repositoryOwner } = await graphql({
         query: `
-        query User($login: String!, $first: Int!) {
-          repositoryOwner(login: $login) {
+        query User($username: String!, $first: Int!) {
+          repositoryOwner(login: $username) {
             login
             avatarUrl
             url
@@ -52,10 +70,10 @@ const App = () => {
           }
         }
       `,
-        login,
+        username,
         first,
         headers: {
-          authorization: `token ${REACT_APP_TOKEN}`,
+          authorization: `token ${token}`,
         },
       });
       setUser(repositoryOwner);
@@ -83,38 +101,46 @@ const App = () => {
     }
   };
 
-  // return (
-  //   <>
-  //     <ToastContainer />
-
-  //     <div className="container">
-  //       <form onSubmit={onSubmit}>
-  //         <input
-  //           onSubmit={onSubmit}
-  //           type="text"
-  //           name="user"
-  //           id="user"
-  //           value={login}
-  //           onChange={(e) => setLogin(e.target.value)}
-  //           placeholder="Type a github username..."
-  //           autoFocus
-  //         />
-  //       </form>
-  //       {user && repos && (
-  //         <div className="result">
-  //           <User user={user} />
-  //           <Repo repos={repos} />
-  //         </div>
-  //       )}
-  //     </div>
-  //   </>
-  // );
+  console.log("token : ", token);
 
   return (
-    <div className="maintenance">
-      <h1>Down for Maintenance</h1>
-      <p>We'll be back soon.</p>
-    </div>
+    <>
+      <ToastContainer />
+
+      {!token && (
+        <div className="login">
+          <img src={logo} alt="GitHub" />
+          <a
+            href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}
+          >
+            <button>login</button>
+          </a>
+        </div>
+      )}
+
+      {token && (
+        <div className="container">
+          <form onSubmit={onSubmit}>
+            <input
+              onSubmit={onSubmit}
+              type="text"
+              name="user"
+              id="user"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Type a github username..."
+              autoFocus
+            />
+          </form>
+          {user && repos && (
+            <div className="result">
+              <User user={user} />
+              <Repo repos={repos} />
+            </div>
+          )}
+        </div>
+      )}
+    </>
   );
 };
 
