89a9ee08f637a982383e452af3ce8f36ca20ad6d
Chaitanya-Raj <chaitanyarajdixit@gmail.com>
2022-07-12 10:19:04 +0000

    Deploying to gh-pages from  @ 1f1f30efba28d8f0f8cdf08e01297fe1ab45176b 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/timeline/precache-manifest.172e4c26b81017c7ae3ea9eb27dcfdc4.js"
+  "/timeline/precache-manifest.395c4a011f3e1751b2a539ac5dd11fc9.js"
 );
 
 self.addEventListener('message', (event) => {
