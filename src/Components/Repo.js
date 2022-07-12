8a458ed75e481d2e85902b17317120601c8bfb2e
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2022-07-12 17:08:48 +0530

    lighten language colors

--- src/Components/Repo.js
+++ src/Components/Repo.js
@@ -26,6 +26,26 @@ const Repo = ({ repos }) => {
     "Dec",
   ];
 
+  const lightenColor = (color, percent) => {
+    var num = parseInt(color.replace("#", ""), 16),
+      amt = Math.round(2.55 * percent),
+      R = (num >> 16) + amt,
+      B = ((num >> 8) & 0x00ff) + amt,
+      G = (num & 0x0000ff) + amt;
+
+    return (
+      "#" +
+      (
+        0x1000000 +
+        (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 +
+        (B < 255 ? (B < 1 ? 0 : B) : 255) * 0x100 +
+        (G < 255 ? (G < 1 ? 0 : G) : 255)
+      )
+        .toString(16)
+        .slice(1)
+    );
+  };
+
   return (
     <div className="timeline-container">
       {repos.map((repo) => {
@@ -39,7 +59,11 @@ const Repo = ({ repos }) => {
               {repo.category.tag && (
                 <span
                   className="tag"
-                  style={{ background: repo.category.color }}
+                  style={{
+                    background: (function () {
+                      return lightenColor(repo.category.color, 20);
+                    })(),
+                  }}
                 >
                   {repo.category.tag}
                 </span>
